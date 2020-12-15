01e204cfc4265e48b3d030620862f90083348cf6
andrey-kupets <andrey.kupets@i.ua>
2020-12-16 00:28:21 +0300

    add self repeat

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {UsersContext} from './contexts/usersContext'
+import {UsersContext} from "./contexts/usersContext";
 
 class App extends React.Component {
 
     render() {
-     const {users,isLoading, posts,isLoading2} = this.context;
-      return (
-            <div className="App">
+        const {users, isLoading, posts, isLoading2} = this.context;
+        return (
+            <div>
                 {
-                    isLoading && <h1>LODING.....users</h1>
+                    isLoading && <h1>LOADING...users</h1>
                 }
-                {
-                    users.map((user) => <h1 key={user.id}>{user.name}</h1>)
-                }
+                {users.map(user => <h1 key={user.id}>{user.name}</h1>)}
                 <hr/>
                 {
-                    isLoading2 && <h1>LODING.....posts</h1>
+                    isLoading2 && <h1>LOADING...posts</h1>
                 }
-                {
-                    posts.map((post) => <p key={post.id}>{post.title}</p>)
-                }
+                {posts.map(post => <p key={post.id}>{post.title}</p>)}
             </div>
-        )
+        );
     }
 }
 
--- src/contexts/usersContext.js
+++ src/contexts/usersContext.js
@@ -1,34 +1,40 @@
-import React,{createContext, Component} from 'react'
+import React, {createContext, Component} from 'react';
 
 export const UsersContext = createContext()
 
-export class UsersProvider extends Component {
+export  class UsersProvider extends Component {
 
     state = {
-        isLoading: false,
+        isLoading: true,
+        isLoading2: true,
         users: [],
-        isLoading2 : false,
         posts: [],
     }
+
     componentDidMount() {
-        this.setState({isLoading: true})
-            fetch('https://jsonplaceholder.typicode.com/users')
-                .then(response=>response.json())
-                .then(users=> this.setState({users,isLoading : false}))
-        this.setState({isLoading2: true})
-            fetch('https://jsonplaceholder.typicode.com/posts')
-                .then(response=>response.json())
-                .then(posts=> this.setState({posts,isLoading2 : false}))
+        this.setState({isLoading: true, isLoading2: true})
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({users, isLoading: false}))
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(posts => this.setState({posts, isLoading2: false}));
     }
 
     render() {
-      return (
-          <UsersContext.Provider value={this.state}>
-              {this.props.children}
-          </UsersContext.Provider>
-      )
-  }
+
+        return (
+            <div>
+                <UsersContext.Provider value={this.state}>
+                    {this.props.children}
+                </UsersContext.Provider>
+            </div>
+        );
+    }
 }
+
+export default UsersContext;
+
 
 // const initialState = {
 //     isLoading: false,
